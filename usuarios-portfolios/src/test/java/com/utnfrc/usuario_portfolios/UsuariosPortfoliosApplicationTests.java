e09d2dd50c3fe799b99124aeb64f4c8086c84266
Sergio Garzon <sggarzonutn@gmail.com>
2026-06-17 20:38:15 -0300

    feat: Resoluciones renegando

---
+++ usuarios-portfolios/src/test/java/com/utnfrc/usuario_portfolios/UsuariosPortfoliosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utnfrc.usuario_portfolios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuariosPortfoliosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
