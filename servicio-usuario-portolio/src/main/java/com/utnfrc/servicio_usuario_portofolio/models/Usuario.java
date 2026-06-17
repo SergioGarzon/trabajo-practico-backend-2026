ef7d9b0d3674c383dc2a364ab732c67b0834e3c8
Sergio Garzon <sggarzonutn@gmail.com>
2026-06-17 17:17:19 -0300

    feat: En progreso microservicio de usuarios

---
+++ servicio-usuario-portolio/src/main/java/com/utnfrc/servicio_usuario_portofolio/models/Usuario.java
@@ -0,0 +1,8 @@
+package com.utnfrc.servicio_usuario_portofolio.models;
+
+public class Usuario {
+	
+	private String nombre;
+	private String email;
+
+}
---
+++ servicio-usuario-portolio/src/test/java/com/utnfrc/servicio_usuario_portofolio/ServicioUsuarioPortolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utnfrc.servicio_usuario_portofolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioUsuarioPortolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
