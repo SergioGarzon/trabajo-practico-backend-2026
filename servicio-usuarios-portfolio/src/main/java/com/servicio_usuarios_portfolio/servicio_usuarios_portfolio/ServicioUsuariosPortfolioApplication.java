69e6643af6b958ed6a4fd5069f46f818c04a3e0f
Sergio Garzon <sggarzonutn@gmail.com>
2026-06-17 15:25:03 -0300

    feat: Microservicio de usuarios, en progreso

---
+++ servicio-usuarios-portfolio/src/main/java/com/servicio_usuarios_portfolio/servicio_usuarios_portfolio/ServicioUsuariosPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.servicio_usuarios_portfolio.servicio_usuarios_portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioUsuariosPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioUsuariosPortfolioApplication.class, args);
+	}
+
+}
---
+++ servicio-usuarios-portfolio/src/test/java/com/servicio_usuarios_portfolio/servicio_usuarios_portfolio/ServicioUsuariosPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servicio_usuarios_portfolio.servicio_usuarios_portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioUsuariosPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
