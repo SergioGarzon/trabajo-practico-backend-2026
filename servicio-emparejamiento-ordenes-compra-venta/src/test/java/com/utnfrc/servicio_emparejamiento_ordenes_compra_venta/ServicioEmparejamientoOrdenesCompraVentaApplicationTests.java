dd8d8aa39658b9224708a57dba651a85b9263cc8
Sergio Garzon <sggarzonutn@gmail.com>
2026-06-21 19:26:38 -0300

    feat: Se  agregar microservicio de ordenes de compra y venta

---
+++ servicio-emparejamiento-ordenes-compra-venta/src/test/java/com/utnfrc/servicio_emparejamiento_ordenes_compra_venta/ServicioEmparejamientoOrdenesCompraVentaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utnfrc.servicio_emparejamiento_ordenes_compra_venta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioEmparejamientoOrdenesCompraVentaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
