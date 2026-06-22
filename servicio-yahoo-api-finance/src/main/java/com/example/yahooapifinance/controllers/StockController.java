ced3d3c4f51af4e48ba5b8dfc184e5a6a75f4f79
Sergio Garzon <sggarzonutn@gmail.com>
2026-06-22 12:24:41 -0300

    fix: Problemas Yahoo Finance

--- servicio-yahoo-api-finance/src/main/java/com/example/yahooapifinance/controllers/StockController.java
+++ servicio-yahoo-api-finance/src/main/java/com/example/yahooapifinance/controllers/StockController.java
@@ -1,35 +1,27 @@
 package com.example.yahooapifinance.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-import java.util.Map;
-
-import org.springframework.web.bind.annotation.GetMapping;
+import com.example.yahooapifinance.services.StockService;
 
 import yahoofinance.Stock;
-import yahoofinance.YahooFinance;
 
 
 @RestController
+@RequestMapping("/api/stocks")
 public class StockController {
 
-	@GetMapping("/saludo")
-	public String saludo() {
-		
-		try {
-			String[] symbols = new String[] {"INTC", "BABA", "TSLA", "AIR.PA", "YHOO"};
-			Map<String, Stock> stocks;
-			stocks = YahooFinance.get(symbols);
-			Stock intel = stocks.get("INTC");
-			Stock airbus = stocks.get("AIR.PA");
-			
-			return intel + "";
-		} catch (IOException e) {
-			
-			e.printStackTrace();
-			return "Error dio";
-		} 
-		
-	}
+	private final StockService stockService;
+
+    public StockController(StockService stockService) {
+        this.stockService = stockService;
+    }
+
+    @GetMapping("/{symbol}")
+    public Stock getStock(@PathVariable String symbol) {
+        return stockService.getStockDetails(symbol.toUpperCase());
+    }
 }
--- servicio-yahoo-api-finance/src/main/java/com/example/yahooapifinance/models/StockWrapper.java
+++
@@ -1,5 +0,0 @@
-package com.example.yahooapifinance.models;
-
-public class StockWrapper {
-
-}
