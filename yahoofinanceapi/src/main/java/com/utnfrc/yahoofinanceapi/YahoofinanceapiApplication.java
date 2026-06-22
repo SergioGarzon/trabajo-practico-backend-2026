8f84b3cd4e0ebb595381847baab2ab5ecadf50dc
Sergio Garzon <sggarzonutn@gmail.com>
2026-06-21 21:11:39 -0300

    feat: Seg agrega microservicio yahoo finances y demas cosas

---
+++ yahoofinanceapi/src/main/java/com/utnfrc/yahoofinanceapi/YahoofinanceapiApplication.java
@@ -0,0 +1,13 @@
+package com.utnfrc.yahoofinanceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YahoofinanceapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YahoofinanceapiApplication.class, args);
+	}
+
+}
---
+++ yahoofinanceapi/src/test/java/com/utnfrc/yahoofinanceapi/YahoofinanceapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utnfrc.yahoofinanceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YahoofinanceapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
