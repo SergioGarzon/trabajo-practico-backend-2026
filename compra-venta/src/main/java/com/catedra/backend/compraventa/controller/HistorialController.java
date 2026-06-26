c220b8875c980168f42ef94e2e77c89b30134a9e
Joaquin <95708@sitemas.frc.utn.edu.ar>
2026-06-26 12:55:45 -0300

    Endpoint Historial

--- compra-venta/src/main/java/com/catedra/backend/compraventa/controller/HistorialController.java
+++ compra-venta/src/main/java/com/catedra/backend/compraventa/controller/HistorialController.java
@@ -4,9 +4,8 @@ import com.catedra.backend.compraventa.dto.TransaccionResponseDto;
 import com.catedra.backend.compraventa.service.TransaccionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,13 +18,11 @@ public class HistorialController {
 
     private final TransaccionService transaccionService;
 
-    @GetMapping("/mis-operaciones")
-    public ResponseEntity<List<TransaccionResponseDto>> misOperaciones(JwtAuthenticationToken jwt) {
-        Long usuarioId = Long.valueOf(jwt.getToken().getSubject());
+    @GetMapping("/usuario/{usuarioId}")
+    public ResponseEntity<List<TransaccionResponseDto>> misOperaciones(@PathVariable Long usuarioId) {
         return ResponseEntity.ok(transaccionService.listarTransaccionesPorUsuario(usuarioId));
     }
 
-    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/todas")
     public ResponseEntity<List<TransaccionResponseDto>> todasLasOperaciones() {
         return ResponseEntity.ok(transaccionService.listarTodasLasTransacciones());
