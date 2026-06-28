dcad6ee172cf5fed0ad4b4aa27b61b39f40f8efc
Gaston <gaston.blangino.03@gmail.com>
2026-06-28 16:06:26 -0300

    modificaciones no muy importante

--- recursos/docker-files/init-db.sql
+++ recursos/docker-files/init-db.sql
@@ -1,2 +1,5 @@
+-- ==========================================
+-- 1. CREACIÓN DE BASES DE DATOS
+-- ==========================================
 CREATE DATABASE IF NOT EXISTS keycloak_db;
 CREATE DATABASE IF NOT EXISTS usuarios_db;
