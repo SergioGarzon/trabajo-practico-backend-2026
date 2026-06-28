17f11f93994945208b18b99851c05131f7dd36a7
Joaquin <95708@sitemas.frc.utn.edu.ar>
2026-06-28 18:04:51 -0300

    Fix-borrado

--- compra-venta/src/main/java/utn/frc/bda/models/OrdenCompra.java
+++ compra-venta/src/main/java/utn/frc/bda/models/OrdenCompra.java
@@ -1,16 +1,19 @@
 package utn.frc.bda.models;
 
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Setter @Getter
 
 @Entity
-@Table(name = "orden_venta")
+@Table(name = "orden_compra")
 public class OrdenCompra {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
