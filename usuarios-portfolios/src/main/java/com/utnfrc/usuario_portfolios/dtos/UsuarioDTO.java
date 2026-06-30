c82e8dd8344df08fccb685af87c26b8bfb9bad1d
Gaston <gaston.blangino.03@gmail.com>
2026-06-29 23:44:54 -0300

    Amen

---
+++ usuarios-portfolios/src/main/java/com/utnfrc/usuario_portfolios/dtos/UsuarioDTO.java
@@ -0,0 +1,22 @@
+package com.utnfrc.usuario_portfolios.dtos;
+
+import com.utnfrc.usuario_portfolios.models.Portfolio;
+import com.utnfrc.usuario_portfolios.services.PortfolioService;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter@Getter@ToString
+@NoArgsConstructor
+public class UsuarioDTO {
+    private String Nombre;
+    private String Apellido;
+    private String Email;
+    private String username;
+    private Double dineroTotal;
+    private Double dineroLibre;
+    private Double dineroBloqueado;
+    private Double dineroInvertido;
+    private Portfolio portfolio;
+}
